52d018bb7fe91d721e336ef7d08ccf2dbc0f198b
Evan McGinnis <evanmc@yahoo.com>
2023-03-19 09:27:55 -0700

    Format corrects to pass tests, although checks for things like too few public methods or too many parameters seems like something that should be tuned

--- assignments/07_syndna/syndna.py
+++ assignments/07_syndna/syndna.py
@@ -11,8 +11,8 @@ import random
 
 def ranged_type(value_type, min_value, max_value):
     """
-    Return function handle of an argument type function for ArgumentParser checking a range:
-        min_value <= arg <= max_value
+    Return function handle of an argument type function for ArgumentParser &
+    checking a range: min_value <= arg <= max_value
 
     Parameters
     ----------
@@ -33,15 +33,17 @@ def ranged_type(value_type, min_value, max_value):
 
     def range_checker(arg: str):
         try:
-            f = value_type(arg)
+            value = value_type(arg)
         except ValueError:
             raise argparse.ArgumentTypeError(f'must be a valid {value_type}')
-        if f < min_value or f > max_value:
-            raise argparse.ArgumentTypeError(f'"{f}" must be between {min_value} and {max_value}')
-        return f
+        if value < min_value or value > max_value:
+            raise argparse.ArgumentTypeError(
+                f'"{value}" must be between {min_value} and {max_value}')
+        return value
 
     # Return function handle to checking function
     return range_checker
+
 
 def create_pool(pctgc, max_len, seq_type):
     """ Create the pool of bases """
@@ -58,17 +60,32 @@ def create_pool(pctgc, max_len, seq_type):
 
 
 class Synthetic:
-    def __init__(self, outfile: str, theType: str, numseqs: int, minlen: int, maxlen: int, percent: float):
+    """
+    Synthetic DNA/RNA
+    """
+    def __init__(self,
+                 outfile: str,
+                 the_type: str,
+                 numseqs: int,
+                 minlen: int,
+                 maxlen: int,
+                 percent: float):
         self._outfile = outfile
-        self._type = theType
+        self._type = the_type
         self._numseqs = numseqs
         self._minlen = minlen
         self._maxlen = maxlen
         self._percent = percent
-        self._pool = create_pool(arguments.pctgc, arguments.maxlen, arguments.seqtype)
+        self._pool = create_pool(arguments.pctgc,
+                                 arguments.maxlen,
+                                 arguments.seqtype)
 
     def write(self) -> bool:
-        with open(self._outfile, "w") as sequences:
+        """
+        Write sequences to file
+        :return:
+        """
+        with open(self._outfile, "w", encoding='UTF-8') as sequences:
             for sequence in range(self._numseqs):
                 seq_length = random.randint(self._minlen, self._maxlen)
                 seq = random.sample(self._pool, seq_length)
@@ -78,31 +95,46 @@ class Synthetic:
         return True
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Assignment 7: Synthetic DNS',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('-o', '--outfile', required=False, type=str, default="out.fa", help="Output filename")
-    parser.add_argument('-t', '--seqtype', required=False, choices=["dna", "rna"], default="dna", type=str, help="dna or rna")
-    parser.add_argument('-n', '--numseqs', required=False, default=10, type=int, help="Number of sequences to create")
-    parser.add_argument('-m', '--minlen', required=False,  default=50, type=int, help="Minimum length")
-    parser.add_argument('-x', '--maxlen', required=False, default=75, type=int, help="Maximum length")
+    parser.add_argument('-o', '--outfile',
+                        required=False,
+                        type=str,
+                        default="out.fa",
+                        help="Output filename")
+    parser.add_argument('-t', '--seqtype',
+                        required=False,
+                        choices=["dna", "rna"],
+                        default="dna",
+                        type=str,
+                        help="dna or rna")
+    parser.add_argument('-n', '--numseqs', required=False, default=10,
+                        type=int, help="Number of sequences to create")
+    parser.add_argument('-m', '--minlen', required=False, default=50, type=int,
+                        help="Minimum length")
+    parser.add_argument('-x', '--maxlen', required=False, default=75, type=int,
+                        help="Maximum length")
 
-    # This is a bit more flexible way of doing this that the method given in the readme
-    # parser.add_argument('-p', '--pctgc', required=False,  type=ranged_type(float, 0, 1), help="Percent GC", default=0.5)
+    # This is a bit more flexible way of doing this that the method given
+    # parser.add_argument('-p', '--pctgc', required=False,
+    # type=ranged_type(float, 0, 1), help="Percent GC", default=0.5)
 
-    parser.add_argument('-p', '--pctgc', required=False,  type=float, help="Percent GC", default=0.5)
-    parser.add_argument('-s', "--seed", required=False, type=int, help="Random seed")
+    parser.add_argument('-p', '--pctgc', required=False, type=float,
+                        help="Percent GC", default=0.5)
+    parser.add_argument('-s', "--seed", required=False, type=int,
+                        help="Random seed")
 
     arguments = parser.parse_args()
 
-    # This is the code that will pass the tests, as the output does not contain a colon
+    # This is the code that will pass the tests,
     if not 0 < arguments.pctgc < 1:
         parser.error(f'--pctgc "{arguments.pctgc}" must be between 0 and 1')
 
-    # If the program requires the seed, the tests won't pass, so see if it is set here.
+    # If the program requires the seed, the tests won't pass, so see
+    # if it is set here.
     if arguments.seed is not None:
         random.seed(arguments.seed)
 
@@ -121,7 +153,10 @@ if __name__ == "__main__":
                       arguments.pctgc)
 
     if synth.write():
-        print(f'Done, wrote {arguments.numseqs} {arguments.seqtype.upper()} sequences to "{arguments.outfile}".')
+        # Assign variables to avoid long lines, so the tests pass
+        seqs_num = arguments.numseqs
+        seqs_type = arguments.seqtype.upper()
+        outname = arguments.outfile
+        print(f'Done, wrote {seqs_num} {seqs_type} sequences to "{outname}".')
 
     sys.exit(0)
-
