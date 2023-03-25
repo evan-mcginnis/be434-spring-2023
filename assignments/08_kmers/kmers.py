585d7da09eefa8af5803e787a67c87496706cc20
Evan McGinnis <evanmc@yahoo.com>
2023-03-25 07:36:43 -0700

    Format changes to pass tests

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -12,6 +12,9 @@ from operator import countOf
 
 
 class Kmer:
+    """
+    K-mer
+    """
     def __init__(self, file: str):
         self._strings = []
         self._exploded = []
@@ -21,7 +24,7 @@ class Kmer:
         """
         Read in the file and convert all lines to a single list
         """
-        with open(self._file) as file:
+        with open(self._file, encoding='UTF-8') as file:
             self._strings = file.read().replace('\n', ' ').split()
 
     def occurences(self, kmer: str) -> int:
@@ -33,6 +36,11 @@ class Kmer:
         return countOf(self._exploded, kmer)
 
     def find_common(self, kmers) -> []:
+        """
+        Find the common strings
+        :param kmers: The kmer to compare
+        :return: list of strings in common
+        """
         set1 = set(self._exploded)
         set2 = set(kmers.exploded)
         intersection = set1.intersection(set2)
@@ -40,6 +48,10 @@ class Kmer:
         return intersection
 
     def generate_all_substrings(self, k: int):
+        """
+        Generate all substrings of the given length
+        :param k: the length of the substring
+        """
         for sub in self._strings:
             res = []
             for i in range(len(sub) - k + 1):
@@ -51,10 +63,18 @@ class Kmer:
 
     @property
     def strings(self) -> []:
+        """
+        The strings associated with an object
+        :return: List of strings
+        """
         return self._strings
 
     @property
     def exploded(self) -> []:
+        """
+        The substrings generated
+        :return: list of substrings
+        """
         return self._exploded
 
 
@@ -73,10 +93,10 @@ if __name__ == "__main__":
     arguments = parser.parse_args()
 
     # Check to see if the files are there
-    for file in arguments.file:
-        target = Path(file)
+    for target in arguments.file:
+        target = Path(target)
         if not target.is_file():
-            print(f"No such file or directory: '{file}'")
+            print(f"No such file or directory: '{target}'")
             sys.exit(-1)
 
     # Check to see that kmers is a positive value
@@ -96,7 +116,5 @@ if __name__ == "__main__":
     in_common_text = right.find_common(left)
 
     for com in in_common_text:
+        # The format numbering is a bit funky here, but the tests pass
         print(f"{com:11}{left.occurences(com):5}{right.occurences(com):6}")
-
-
-
